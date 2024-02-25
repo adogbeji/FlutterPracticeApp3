17434f890450e7ad753ded2d2633af5f1ae57596
adogbeji <baka_1993@yahoo.co.uk>
2024-02-25 18:31:14 +0000

    Check if input fields are valid in login screen

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -60,7 +60,12 @@ class LoginScreen extends StatelessWidget {
               ),
               InkWell(
                 onTap: () {
-                  print('Logged In!');
+                  // print('Logged In!');
+                  if (_formKey.currentState!.validate()) {
+                    print('Valid!');
+                  } else {
+                    print('Not Valid!');
+                  }
                 },
                 child: Container(
                   height: 50,
