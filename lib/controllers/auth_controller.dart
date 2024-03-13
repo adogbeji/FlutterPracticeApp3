6b4e86791a812361c879f3360b217550067e15e9
adogbeji <baka_1993@yahoo.co.uk>
2024-03-13 13:46:31 +0000

    Create new variable * add try-catch block in AuthController class

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<String> createNewUser(String fullName, String email, String password) async {
     String res = 'Some error occured!';
+
+    try {
+
+    } catch (e) {}
 
     return res;
   }
