2ef99c68b2a644cb08e6074df7326622c696341f
adogbeji <baka_1993@yahoo.co.uk>
2024-05-13 13:38:33 +0100

    Add more styling for login buton in login screen

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -9,13 +11,18 @@ class LoginScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          const Text('Log In', style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 4,
-          ),),
+          const Text(
+            'Log In',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 4,
+            ),
+          ),
 
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
 
           // EMAIL INPUT FIELD
           TextFormField(
@@ -24,15 +31,17 @@ class LoginScreen extends StatelessWidget {
             ),
           ),
 
-          const SizedBox(height: 20,),
-          
+          const SizedBox(
+            height: 20,
+          ),
+
           // PASSWORD INPUT FIELD
           TextFormField(
             decoration: const InputDecoration(
               labelText: 'Password',
             ),
           ),
-          
+
           // LOGIN BUTTON
           Container(
             width: MediaQuery.of(context).size.width - 40,
@@ -40,6 +49,17 @@ class LoginScreen extends StatelessWidget {
             decoration: BoxDecoration(
               color: Colors.pink,
               borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Center(
+              child: Text(
+                'Log In',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 4,
+                ),
+              ),
             ),
           ),
         ],
