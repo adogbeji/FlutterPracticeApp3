defdcb9a9ea33b3aec9b0d55e38ab6e6344526f9
adogbeji <baka_1993@yahoo.co.uk>
2024-06-11 13:39:53 +0100

    Add text & border radius to register button in register screen

--- lib/views/screens/auth/register_screen.dart
+++ lib/views/screens/auth/register_screen.dart
@@ -18,45 +18,64 @@ class RegisterScreen extends StatelessWidget {
             ),
           ),
 
-          const SizedBox(height: 20,),
-          
+          const SizedBox(
+            height: 20,
+          ),
+
           // NAME INPUT FIELD
           TextFormField(
             decoration: const InputDecoration(
               labelText: 'Name',
               hintText: 'Enter Name',
-              prefixIcon: Icon(Icons.person, color: Colors.pink,),
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.pink,
+              ),
             ),
           ),
 
-          const SizedBox(height: 20,),
-          
+          const SizedBox(
+            height: 20,
+          ),
+
           // EMAIL INPUT FIELD
           TextFormField(
             decoration: const InputDecoration(
               labelText: 'Email',
               hintText: 'Enter Email Address',
-              prefixIcon: Icon(Icons.email, color: Colors.pink,),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.pink,
+              ),
             ),
           ),
 
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
 
           // PASSWORD INPUT FIELD
           TextFormField(
             decoration: const InputDecoration(
               labelText: 'Password',
               hintText: 'Enter Password',
-              prefixIcon: Icon(Icons.lock, color: Colors.pink,),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.pink,
+              ),
             ),
           ),
 
           // REGISTER BUTTON
           Container(
-            width: MediaQuery.of(context).size.width,
+            width: MediaQuery.of(context).size.width - 40,
             height: 52,
-            decoration: const BoxDecoration(
+            decoration: BoxDecoration(
               color: Colors.pink,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const Center(
+              child: Text('Register'),
             ),
           ),
         ],
